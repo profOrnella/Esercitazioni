ccb702ba920099eee2301e3ce6f51e4509e20db6
profOrnella <profandrisani@gmail.com>
2021-07-02 10:34:19 +0200

    prima commit

---
+++ script/variable.js
@@ -0,0 +1,17 @@
+
+/*
+CALCOLO DELL'AREA DI UN RETTANGOLO
+*/
+var base=10;
+var altezza= 20
+var area=base*altezza;
+
+let x = 5;      // dichiaro x e le assegno il valore 5
+let y = x + 2;  // dichiaro y e le assegno il valore x+2
+
+
+example=222;
+console.log(example);
+
+const PI=3.14;
+PI=5; //DA ERRORE A RUNTIME
